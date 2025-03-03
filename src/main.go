c33cd00ea675c3a5c4fec79e31de8e1b72921621
Rodrigo Silvério <silverio_tenor@outlook.com>
2025-03-03 16:14:56 -0300

    Fixing go version and setup DB

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/silverioTenor/fc-arq-hexagonal-go/src/adapter/db"
@@ -12,5 +13,12 @@ func main() {
 	dbConn, _ := sql.Open("sqlite3", "db.sqlite")
 	productDbAdapter := db.NewProductDb(dbConn)
 	productService := service.NewProductService(productDbAdapter)
-	productService.Create("Product 1", 10)
+	product, _ := productService.Create("Product 1", 10)
+	// product, _ := productService.Get("c1e5fc2d-f660-4f0b-a2a9-58dd9318a81a")
+
+	_, err := productService.Toggle(product)
+
+	if (err != nil) {
+		fmt.Println(err)
+	}
 }
